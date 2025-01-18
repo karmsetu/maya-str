f37c4c138ab8f996f4b7f8f7192d9aaa16eaf948
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-01-18 16:11:15 +0530

    error #1: mermaid hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-}
+    title: 'MayaStr',
+    description: 'Redifining Encryption',
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
